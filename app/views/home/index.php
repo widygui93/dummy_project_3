9f794eebe35e7984c3990caeed392db07944df28
widygui93 <guilias.widy@gmail.com>
2020-09-09 19:57:24 +0700

    di file app/views/home/index.php buat kerangka tag html nya

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -13,11 +13,327 @@
     		    <div class="menu-btn__burger"></div>
     		</div>
     		<span>YouAndMe</span>
+    		<div class="menu-navbar">
+    			<ul>
+    				<li>Article</li>
+    				<li>About</li>
+    				<li>Contact</li>
+    				<li>My Dashboard</li>
+    			</ul>
+    		</div>
     		<a href="#">Log in</a>
     		<a href="#">Sign Up</a>
 		</header>
-		<main class="main"></main>
-		<footer class="footer"></footer>
+		<main class="main">
+			<div class="call-to-action">
+				<h1>Welcome to YouAndMe</h1>
+				<span>Learing Platform</span>
+				<button>Register</button>
+			</div>
+			<div class="latest-article">
+				<h3>Latest Article</h3>
+				<div class="article">
+					<div class="article-head">
+						<img src="" alt="article-img">
+					</div>
+					<div class="article-main">
+						<h5 class="article-title">Title 1</h5>
+						<span class="article-author">By Author</span>
+						<small class="article-like">23K</small>
+						<small class="article-purchase">45K</small>
+					</div>
+				</div>
+				<div class="article">
+					<div class="article-head">
+						<img src="" alt="article-img">
+					</div>
+					<div class="article-main">
+						<h5 class="article-title">Title 2</h5>
+						<span class="article-author">By Author</span>
+						<small class="article-like">23K</small>
+						<small class="article-purchase">45K</small>
+					</div>
+				</div>
+				<div class="article">
+					<div class="article-head">
+						<img src="" alt="article-img">
+					</div>
+					<div class="article-main">
+						<h5 class="article-title">Title 3</h5>
+						<span class="article-author">By Author</span>
+						<small class="article-like">23K</small>
+						<small class="article-purchase">45K</small>
+					</div>
+				</div>
+			</div>
+			<div class="student-testi">
+				<h3>Student Testimony</h3>
+				<div class="testi">
+					<div class="testi-head">
+						<img src="" alt="testi-img">
+					</div>
+					<div class="testi-main">
+						<span class="student-name">John</span>
+						<p class="testi-text">In YouAndMe, I want to learn whenever I can. If it's still unclear, the article can also be re-read "</p>
+					</div>
+				</div>
+				<div class="testi">
+					<div class="testi-head">
+						<img src="" alt="testi-img">
+					</div>
+					<div class="testi-main">
+						<span class="student-name">John</span>
+						<p class="testi-text">In YouAndMe, I want to learn whenever I can. If it's still unclear, the article can also be re-read "</p>
+					</div>
+				</div>
+				<div class="testi">
+					<div class="testi-head">
+						<img src="" alt="testi-img">
+					</div>
+					<div class="testi-main">
+						<span class="student-name">John</span>
+						<p class="testi-text">In YouAndMe, I want to learn whenever I can. If it's still unclear, the article can also be re-read "</p>
+					</div>
+				</div>
+				<div class="testi">
+					<div class="testi-head">
+						<img src="" alt="testi-img">
+					</div>
+					<div class="testi-main">
+						<span class="student-name">John</span>
+						<p class="testi-text">In YouAndMe, I want to learn whenever I can. If it's still unclear, the article can also be re-read "</p>
+					</div>
+				</div>
+				<div class="testi">
+					<div class="testi-head">
+						<img src="" alt="testi-img">
+					</div>
+					<div class="testi-main">
+						<span class="student-name">John</span>
+						<p class="testi-text">In YouAndMe, I want to learn whenever I can. If it's still unclear, the article can also be re-read "</p>
+					</div>
+				</div>
+			</div>
+			<div class="list-teacher">
+				<h3>Teacher List</h3>
+				<div class="teacher">
+					<div class="teacher-head">
+						<img src="" alt="teacher-img">
+					</div>
+					<div class="teacher-main">
+						<span class="teacher-name">John</span>
+						<span class="teacher-specialist">Economy</span>
+					</div>
+				</div>
+				<div class="teacher">
+					<div class="teacher-head">
+						<img src="" alt="teacher-img">
+					</div>
+					<div class="teacher-main">
+						<span class="teacher-name">Erik</span>
+						<span class="teacher-specialist">Sosiology</span>
+					</div>
+				</div>
+				<div class="teacher">
+					<div class="teacher-head">
+						<img src="" alt="teacher-img">
+					</div>
+					<div class="teacher-main">
+						<span class="teacher-name">Kane</span>
+						<span class="teacher-specialist">Math</span>
+					</div>
+				</div>
+			</div>
+			<div class="comparison">
+				<h3>Why use YouAndMe</h3>
+				<div class="comparison-table">
+					<div class="comparison-table-head">
+						<button>Konventional</button>
+						<button>YouAndMe</button>
+					</div>
+					<div class="comparison-table-main">
+						<div class="konven-tab">
+							<table>
+								<tr>
+								  <th>Company</th>
+								  <th>Contact</th>
+								  <th>Country</th>
+								</tr>
+								<tr>
+								  <td>Alfreds Futterkiste</td>
+								  <td>Maria Anders</td>
+								  <td>Germany</td>
+								</tr>
+								<tr>
+								  <td>Centro comercial Moctezuma</td>
+								  <td>Francisco Chang</td>
+								  <td>Mexico</td>
+								</tr>
+								<tr>
+								  <td>Ernst Handel</td>
+								  <td>Roland Mendel</td>
+								  <td>Austria</td>
+								</tr>
+								<tr>
+								  <td>Island Trading</td>
+								  <td>Helen Bennett</td>
+								  <td>UK</td>
+								</tr>
+								<tr>
+								  <td>Laughing Bacchus Winecellars</td>
+								  <td>Yoshi Tannamuri</td>
+								  <td>Canada</td>
+								</tr>
+								<tr>
+								  <td>Magazzini Alimentari Riuniti</td>
+								  <td>Giovanni Rovelli</td>
+								  <td>Italy</td>
+								</tr>
+							</table>
+						</div>
+						<div class="youandme-tab">
+							<table>
+								<tr>
+								  <th>Company</th>
+								  <th>Contact</th>
+								  <th>Country</th>
+								</tr>
+								<tr>
+								  <td>Island Trading</td>
+								  <td>Helen Bennett</td>
+								  <td>UK</td>
+								</tr>
+								<tr>
+								  <td>Laughing Bacchus Winecellars</td>
+								  <td>Yoshi Tannamuri</td>
+								  <td>Canada</td>
+								</tr>
+								<tr>
+								  <td>Magazzini Alimentari Riuniti</td>
+								  <td>Giovanni Rovelli</td>
+								  <td>Italy</td>
+								</tr>
+								<tr>
+								  <td>Alfreds Futterkiste</td>
+								  <td>Maria Anders</td>
+								  <td>Germany</td>
+								</tr>
+								<tr>
+								  <td>Centro comercial Moctezuma</td>
+								  <td>Francisco Chang</td>
+								  <td>Mexico</td>
+								</tr>
+								<tr>
+								  <td>Ernst Handel</td>
+								  <td>Roland Mendel</td>
+								  <td>Austria</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer class="footer">
+			<div class="col-12 bg-dark text-white pb-3 pt-5">
+				<div class="row">
+					<div class="col-lg-2 col-sm-4 text-center text-sm-left mb-sm-0 mb-3">
+	                    <div class="row">
+	                        <div class="col-12">
+	                            <div class="footer-logo">
+	                                <a href="index.php">E-Commerce</a>
+	                            </div>
+	                        </div>
+	                        <div class="col-12">
+	                            <address>
+	                                221B Baker Street<br>
+	                                London, England
+	                            </address>
+	                        </div>
+	                    </div>
+                	</div>
+	                <div class="col-lg-3 col-sm-8 text-center text-sm-left mb-sm-0 mb-3">
+	                    <div class="row">
+	                        <div class="col-12 text-uppercase">
+	                            <h4>Who are we?</h4>
+	                        </div>
+	                        <div class="col-12 text-justify">
+	                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet vel ligula vel sodales. Aenean vel ullamcorper purus, ac pharetra arcu. Nam enim velit, ultricies eu orci nec, aliquam efficitur sem. Quisque in sapien a sem vestibulum volutpat at eu nibh. Suspendisse eget est metus. Maecenas mollis quis nisl ac malesuada. Donec gravida tortor massa, vitae semper leo sagittis a. Donec augue turpis, rutrum vitae augue ut, venenatis auctor nulla. Sed posuere at erat in consequat. Nunc congue justo ut ante sodales, bibendum blandit augue finibus.</p>
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="col-lg-2 col-sm-3 col-5 ml-lg-auto ml-sm-0 ml-auto mb-sm-0 mb-3">
+	                    <div class="row">
+	                        <div class="col-12 text-uppercase">
+	                            <h4>Quick Links</h4>
+	                        </div>
+	                        <div class="col-12">
+	                            <ul class="footer-nav">
+	                                <li>
+	                                    <a href="#">Home</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Contact Us</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">About Us</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Privacy Policy</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Terms & Conditions</a>
+	                                </li>
+	                            </ul>
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="col-lg-1 col-sm-2 col-4 mr-auto mb-sm-0 mb-3">
+	                    <div class="row">
+	                        <div class="col-12 text-uppercase text-underline">
+	                            <h4>Help</h4>
+	                        </div>
+	                        <div class="col-12">
+	                            <ul class="footer-nav">
+	                                <li>
+	                                    <a href="#">FAQs</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Shipping</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Returns</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Track Order</a>
+	                                </li>
+	                                <li>
+	                                    <a href="#">Report Fraud</a>
+	                                </li>
+	                            </ul>
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="col-lg-3 col-sm-6 text-center text-sm-left">
+	                    <div class="row">
+	                        <div class="col-12 text-uppercase">
+	                            <h4>Newsletter</h4>
+	                        </div>
+	                        <div class="col-12">
+	                            <form action="#">
+	                                <div class="form-group">
+	                                    <input type="text" class="form-control" placeholder="Enter your email..." required>
+	                                </div>
+	                                <div class="form-group">
+	                                    <button class="btn btn-outline-light text-uppercase">Subscribe</button>
+	                                </div>
+	                            </form>
+	                        </div>
+	                    </div>
+	                </div>
+				</div>
+			</div>
+		</footer>
 	</div>
 <script src="<?= BASEURL; ?>/js/jquery-3.4.1.min.js"></script>
 <script src="<?= BASEURL; ?>/js/script.js"></script>
