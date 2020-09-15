ffbecb0e0424ce4688b97fba571f70cca069bbb0
widygui93 <guilias.widy@gmail.com>
2020-09-15 22:30:12 +0700

    di file script.js hapus comment aja, di file style.css tukar posisi logo-wrapper dan menu-btn saat mobile view dan buat media query saat width 748px maka hide menu-btn, show menu-navbar dan login-signup-wrapper secara horizontal

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,5 @@
 $(function(){
 	$(".menu-btn").on('click',function(){
-		// $(".menu-admin").toggleClass("aktif");
-		// if($(".menu-admin").hasClass('aktif')) {
-		// 	$(".menu-admin").removeClass('aktif');
-		// 	$(".menu-admin").addClass('inaktif');
-		// } else {
-		// 	$(".menu-admin").removeClass('inaktif');
-		// 	$(".menu-admin").addClass('aktif');
-		// } 
 		$(".menu-btn").toggleClass("open");
 		$(".header").toggleClass("open");
 		$(".menu-navbar").toggleClass("open");
