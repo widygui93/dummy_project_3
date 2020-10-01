98c9c67e47b4a293ca909bcc769a32bb17d53761
widygui93 <guilias.widy@gmail.com>
2020-10-01 21:24:24 +0700

    di file home/index.php dan style.scss buat pagination secara hardcode dan kasih style awal dulu

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -267,6 +267,16 @@
 				</div>
 			</div>
 		</div>
+		<ul>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#">6</a></li>
+			<li><a href="#">7</a></li>
+			<li><a href="#">8</a></li>
+		</ul>
 	</div>
 	<div class="student-testi">
 		<h3>Student Testimony</h3>
