cd8a5db13145754c7cdd31cbb0087d739af1c849
widygui93 <guilias.widy@gmail.com>
2020-11-22 12:45:18 +0700

    hapus config.php

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/widy/project/dummy_project_3/public');
-
-//DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'phpmvc');
