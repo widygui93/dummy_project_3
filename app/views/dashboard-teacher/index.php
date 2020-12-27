fdc0e6a015ef5406f1cf8af8b5dadc317a9428ac
widygui93 <guilias.widy@gmail.com>
2020-12-27 20:50:33 +0700

    di file dashboard-teacher-style.scss buat role class tutorial-display yg contek dari role class latest-tutorial. di file dashboard-teacher/index.php isi tutorial-display dengan tutorial dari home/index.php cuma hilangkan author dan ganti button purchase jadi button play

--- app/views/dashboard-teacher/index.php
+++ app/views/dashboard-teacher/index.php
@@ -70,5 +70,252 @@
         </form>
     </div>
     <div class="tutorial-display">
+        <h3>Your Tutorial</h3>
+        <div class="tutorial-wrap">
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+            <div class="tutorial">
+				<div class="tutorial-video">
+					<img src="../app/core/videos/cover-img/sample.png" alt="video-poster">
+				</div>
+				<div class="tutorial-info">
+					<div class="info-1">
+						<span class="tutorial-title"><a href="#">Learning X for Beginner</a></span>
+						<span class="tooltiptext">Click for details</span>
+					</div>
+					<div class="info-2">
+						<small class="tutorial-date">September 25,2020</small>
+					</div>
+				</div>
+				<div class="tutorial-play">
+					<div class="play-button">
+						<a href="#">Play</a>
+					</div>
+					<div class="play-info">
+						<span class="tutorial-like">
+							<img src="<?= BASEURL; ?>/svg/Green_Heart_Icon.svg" alt="like">
+							<span>23K</span>
+						</span>
+						<span class="tutorial-cost">
+							<img src="<?= BASEURL; ?>/svg/green_dollar_icon.svg" alt="cost">
+							<span>45K</span>
+						</span>
+					</div>
+				</div>
+            </div>
+        </div>
+        <div class="tutorial-page">
+            <ul>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">6</a></li>
+                <li><a href="#">7</a></li>
+                <li><a href="#">8</a></li>
+            </ul>
+        </div>
     </div>
 </main>
