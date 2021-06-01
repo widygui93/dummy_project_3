133d2836a395c56b4aadfeaf47f9f234febe7915
widygui93 <guilias.widy@gmail.com>
2021-06-01 18:30:04 +0700

    di template header tambahkan url utk action dan ganti method jadi POST. buat file controller Search, view search/index.html.twig,dan search-style.scss

---
+++ app/controllers/Search.php
@@ -0,0 +1,47 @@
+<?php
+class Search extends Controller{
+    public function index(){
+		$data['style'] = BASEURL.'/css/search-style.css';
+        $data['style-tutorial'] = BASEURL.'/css/tutorial-style.css';
+        $data['script-modal-detail-tutorial'] = BASEURL.'/js/script-modal-detail-tutorial.js';
+		$data['script-axios'] = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
+		$data['script-load-more'] = BASEURL.'/js/script-load-more.js';
+		$data['tutorials'] = $this->model('Search_model')->getTutorials();
+		$data['total-tutorials'] = $this->model('Search_model')->getTotalTutorials();
+
+        
+        $twig = $this->view();
+
+        echo $twig->render('/templates/header.html.twig', 
+			[
+				'style' => $data['style'], 
+				'style_tutorial' => $data['style-tutorial'], 
+				'BASEURL' => BASEURL,
+				'login_teacher' => $_SESSION['login_teacher'] ?? false,
+				'login_student' => $_SESSION['login_student'] ?? false,
+				'username_teacher' => $_SESSION['username_teacher'] ?? '',
+				'username_student' => $_SESSION['username_student'] ?? ''
+			]
+		);
+
+        echo $twig->render('/search/index.html.twig',
+            [
+                // 'BASEURL' => BASEURL,
+                // 'q' => $_POST['q']
+				'tutorials' => $data['tutorials'], 
+				'total_tutorials' => $data['total-tutorials'],
+				'BASEURL' => BASEURL
+            ]
+        );
+
+        echo $twig->render('/templates/footer.html.twig',
+			[
+				'script_axios' => $data['script-axios'],
+                'script_modal_detail_tutorial' => $data['script-modal-detail-tutorial'],
+                'script_load_more' => $data['script-load-more'],
+				'BASEURL' => BASEURL 
+			]
+		);
+
+    }
+}
