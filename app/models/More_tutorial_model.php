a2a3a0e80ca2bcd8537746fea318e8848d5a0c75
widygui93 <guilias.widy@gmail.com>
2021-06-12 19:38:18 +0700

    di file More_tutorial_model.php di method getMoreTutorialsBy ganti query pake binding

--- app/models/More_tutorial_model.php
+++ app/models/More_tutorial_model.php
@@ -26,7 +26,7 @@ class More_tutorial_model extends Model
                         tutorial.is_revoke,
                         liked_tutorial.liked_by
                     FROM tutorial JOIN liked_tutorial ON tutorial.id = liked_tutorial.tutorial_id
-                    WHERE tutorial.created_by = '" . $username . "'
+                    WHERE tutorial.created_by = :username
                 ) AS tbl_tutorial
                 GROUP BY id,title, img_cover, created_by , prize,created_date, tutorial_date, is_revoke
                 UNION
@@ -42,13 +42,13 @@ class More_tutorial_model extends Model
                             tutorial.is_revoke,
                             liked_tutorial.liked_by
                         FROM tutorial LEFT JOIN liked_tutorial ON tutorial.id = liked_tutorial.tutorial_id
-                        WHERE tutorial.created_by = '" . $username . "' AND liked_by IS NULL
+                        WHERE tutorial.created_by = :username AND liked_by IS NULL
                     ) AS tbl_tutorial
                 ORDER BY tutorial_date DESC
-                LIMIT " . $tutorialsPerPage . " OFFSET " . $startIndexOfMoreTutorials . "
+                LIMIT :tutorialsPerPage OFFSET :startIndexOfMoreTutorials
             ";
 
-            $tutorials = R::getAll($query);
+            $tutorials = R::getAll($query, [':username' => $username, ':tutorialsPerPage' => $tutorialsPerPage, ':startIndexOfMoreTutorials' => $startIndexOfMoreTutorials]);
 
             $tutorials = $this->shortenTitle($tutorials);
 
