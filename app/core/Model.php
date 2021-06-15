fa0c29cb3bda3c2dcac4498df3df52a39de33ca7
widygui93 <guilias.widy@gmail.com>
2021-06-15 21:17:21 +0700

    di Model.php buat binding utk method getMoreTutorialsFromModelClass

--- app/core/Model.php
+++ app/core/Model.php
@@ -1,21 +1,23 @@
 <?php
 
-class Model {
+class Model
+{
     private $db;
-    public function createRandomString(): string {
+    public function createRandomString(): string
+    {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $randomID = '';
 
-        for($i = 0; $i < 30; $i++){
+        for ($i = 0; $i < 30; $i++) {
             $index = rand(0, strlen($characters) - 1);
-            $randomID = $randomID . substr($characters,$index,1);
+            $randomID = $randomID . substr($characters, $index, 1);
         }
 
         return $randomID;
-
     }
-    
-    public function getDate(): string {
+
+    public function getDate(): string
+    {
         return date("Y-m-d H:i:s");
     }
 
@@ -26,13 +28,15 @@ class Model {
     //     return empty($data) ? true : false;
     // }
 
-    public function isDataEmpty(array $data): bool{
+    public function isDataEmpty(array $data): bool
+    {
         return empty($data) ? true : false;
     }
 
-    public function doesMandatoryDataFilled(array $data): bool {
-        foreach ($data as $val){
-            if( strlen($val) == 0 ){
+    public function doesMandatoryDataFilled(array $data): bool
+    {
+        foreach ($data as $val) {
+            if (strlen($val) == 0) {
                 return false;
                 break;
             }
@@ -40,45 +44,53 @@ class Model {
         return true;
     }
 
-    public function purify($data){
+    public function purify($data)
+    {
         return htmlspecialchars($data);
     }
 
-    public function isUsernameExist(string $username, string $table): bool{
-        $numOfUser = R::count( $table, ' username = ? ' , [ $username ] );
+    public function isUsernameExist(string $username, string $table): bool
+    {
+        $numOfUser = R::count($table, ' username = ? ', [$username]);
         return $numOfUser > 0 ? true : false;
     }
 
-    public function isBreak(string $data, string $pattern): bool{
+    public function isBreak(string $data, string $pattern): bool
+    {
         return preg_match($pattern, $data) === 1 ?  false :  true;
     }
 
-    public function isNotMatch(string $password, string $password_confirm): bool{
+    public function isNotMatch(string $password, string $password_confirm): bool
+    {
         return $password !== $password_confirm ? true : false;
     }
 
-    public function isIdNotAvailable(string $id): bool{
-        return R::count( 'tutorial', ' id = :id ', [ ':id' => $id ] ) > 0  ? false : true;
+    public function isIdNotAvailable(string $id): bool
+    {
+        return R::count('tutorial', ' id = :id ', [':id' => $id]) > 0  ? false : true;
     }
 
-    public function isIDNotUUID(string $id): bool{
+    public function isIDNotUUID(string $id): bool
+    {
         return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/', $id) === 1 ? false : true;
     }
 
-    public function isIneligibleTutorial(string $id): bool {
-        if( !isset($_SESSION["login_teacher"]) && !isset($_SESSION["login_student"]) ){
+    public function isIneligibleTutorial(string $id): bool
+    {
+        if (!isset($_SESSION["login_teacher"]) && !isset($_SESSION["login_student"])) {
             return false;
         }
-        $tutorials = R::find( 'tutorial', ' id = :id and created_by = :created_by ', [ ':id' => $id, ':created_by' => $_SESSION["username_teacher"] ]);
+        $tutorials = R::find('tutorial', ' id = :id and created_by = :created_by ', [':id' => $id, ':created_by' => $_SESSION["username_teacher"]]);
         return empty($tutorials) ? true : false;
     }
 
 
-    public function shortenTitle(array $tutorialSets): array{
+    public function shortenTitle(array $tutorialSets): array
+    {
         $maxChars = 23; // max jumlah karakter yang di tampilkan di browser
         $charLimit = 20; // batas karakter sebelum ...
-        foreach($tutorialSets as &$tutorialSet ){
-            if( strlen($tutorialSet['title']) > $maxChars ){
+        foreach ($tutorialSets as &$tutorialSet) {
+            if (strlen($tutorialSet['title']) > $maxChars) {
                 $tutorialSet['title'] = substr($tutorialSet['title'], 0, $charLimit);
                 $tutorialSet['title'] = $tutorialSet['title'] . '...';
             }
@@ -87,10 +99,11 @@ class Model {
         return $tutorialSets;
     }
 
-    public function getTutorialsFromModelClass(): array{
+    public function getTutorialsFromModelClass(): array
+    {
         $tutorialsPerPage = 4;
-        $numOfTutorials = R::count( 'tutorial' );
-        if( $numOfTutorials > 0 ){
+        $numOfTutorials = R::count('tutorial');
+        if ($numOfTutorials > 0) {
             $query = "
                 SELECT COUNT(id) AS total_like, id, title, img_cover, created_by, prize, created_date, tutorial_date, is_revoke
                 FROM (
@@ -126,28 +139,27 @@ class Model {
                 LIMIT " . $tutorialsPerPage . "
             ";
 
-            $tutorials = R::getAll( $query );
+            $tutorials = R::getAll($query);
 
             $tutorials = $this->shortenTitle($tutorials);
 
             return $tutorials;
-            
-
         } else {
             $tutorials = array();
             return $tutorials;
         }
-
     }
 
-    public function getTotalTutorialsFromModelClass(): int{
-        return R::count( "tutorial", " is_revoke = 'N' " );
+    public function getTotalTutorialsFromModelClass(): int
+    {
+        return R::count("tutorial", " is_revoke = 'N' ");
     }
 
-    public function getMoreTutorialsFromModelClass(int $startIndexOfMoreTutorials): array{
+    public function getMoreTutorialsFromModelClass(int $startIndexOfMoreTutorials): array
+    {
         $tutorialsPerPage = 4;
-        $numOfTutorials = R::count( 'tutorial' );
-        if( $numOfTutorials > 0 ){
+        $numOfTutorials = R::count('tutorial');
+        if ($numOfTutorials > 0) {
             $query = "
                 SELECT COUNT(id) AS total_like, id, title, img_cover, created_by, prize, created_date, tutorial_date, is_revoke
                 FROM (
@@ -180,21 +192,17 @@ class Model {
                         WHERE tutorial.is_revoke = 'N' AND liked_by IS NULL
                     ) AS tbl_tutorial
                 ORDER BY tutorial_date DESC
-                LIMIT " . $tutorialsPerPage . " OFFSET " . $startIndexOfMoreTutorials . "
+                LIMIT :tutorialsPerPage OFFSET :startIndexOfMoreTutorials
             ";
 
-            $tutorials = R::getAll( $query );
+            $tutorials = R::getAll($query, [':tutorialsPerPage' => $tutorialsPerPage, ':startIndexOfMoreTutorials' => $startIndexOfMoreTutorials]);
 
             $tutorials = $this->shortenTitle($tutorials);
 
             return $tutorials;
-            
-
         } else {
             $tutorials = array();
             return $tutorials;
         }
-
     }
-
 }
