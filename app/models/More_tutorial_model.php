a0d3fa065a2a547ba884e220e2e78c6bd785a9bf
widygui93 <guilias.widy@gmail.com>
2021-06-27 20:48:53 +0700

    di file More_tutorial_model.php pada saat hasil search title dgn like tidak ada maka gunakan searchwithLevenshteinDistance yg ada di Model.php kemudian cari jlhIterasi agar tahu kpn mesti berhenti query data ke DB

--- app/models/More_tutorial_model.php
+++ app/models/More_tutorial_model.php
@@ -111,6 +111,56 @@ class More_tutorial_model extends Model
             return $tutorials;
         } else {
             $tutorials = array();
+            $listLevenshteinDistances = $this->searchWithLevenshteinDistance($keyword);
+
+            $jlhIterasi = 0;
+            if (($tutorialsPerPage + $startIndexOfMoreTutorials) < count($listLevenshteinDistances)) {
+                $jlhIterasi = $tutorialsPerPage;
+            } else {
+                $jlhIterasi = count($listLevenshteinDistances) - $startIndexOfMoreTutorials;
+            }
+
+            for ($i = 0; $i < $jlhIterasi; $i++) {
+
+                $query = "
+                    SELECT COUNT(id) AS total_like, id, title, img_cover, created_by, prize, created_date, tutorial_date, is_revoke
+                    FROM (
+                        SELECT tutorial.id,
+                            tutorial.title,
+                            tutorial.img_cover,
+                            tutorial.created_by,
+                            to_char(tutorial.prize, '999,999,999') AS prize,
+                            to_char(tutorial.created_date, 'Month DD,YYYY') AS created_date,
+                            tutorial.created_date AS tutorial_date,
+                            tutorial.is_revoke,
+                            liked_tutorial.liked_by
+                        FROM tutorial JOIN liked_tutorial ON tutorial.id = liked_tutorial.tutorial_id
+                        WHERE tutorial.id = :id
+                    ) AS tbl_tutorial
+                    GROUP BY id,title, img_cover, created_by , prize,created_date, tutorial_date, is_revoke
+                    UNION
+                    SELECT 0 AS total_like,id, title, img_cover, created_by , prize,created_date, tutorial_date, is_revoke
+                        FROM (
+                            SELECT tutorial.id,
+                                tutorial.title,
+                                tutorial.img_cover,
+                                tutorial.created_by,
+                                to_char(tutorial.prize, '999,999,999') AS prize,
+                                to_char(tutorial.created_date, 'Month DD,YYYY') AS created_date,
+                                tutorial.created_date AS tutorial_date,
+                                tutorial.is_revoke,
+                                liked_tutorial.liked_by
+                            FROM tutorial LEFT JOIN liked_tutorial ON tutorial.id = liked_tutorial.tutorial_id
+                            WHERE tutorial.id = :id AND liked_by IS NULL
+                        ) AS tbl_tutorial
+                ";
+
+                $tutorial = R::getAll($query, [':id' => $listLevenshteinDistances[$startIndexOfMoreTutorials]['id']]);
+                array_push($tutorials, $tutorial[0]);
+                $startIndexOfMoreTutorials++;
+            }
+
+            $tutorials = $this->shortenTitle($tutorials);
             return $tutorials;
         }
     }
