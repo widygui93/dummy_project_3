893be47917408a1d0bd33aa23b76baca8bbdd561
widygui93 <guilias.widy@gmail.com>
2021-07-10 12:09:10 +0700

    buat method getMoreTutorialsforBestSeller

--- app/models/More_tutorial_model.php
+++ app/models/More_tutorial_model.php
@@ -162,4 +162,56 @@ class More_tutorial_model extends Model
             return $tutorials;
         }
     }
+
+    public function getMoreTutorialsForBestSeller(int $startIndexOfMoreTutorials): array
+    {
+        $numOfTutorials = R::count('tutorial');
+        if ($numOfTutorials > 0) {
+            $query = "
+                SELECT 
+                    COUNT(id_like_tutorial) AS total_like,
+                    COUNT(id_purchase_tutorial) AS total_purchase,
+                    title,
+                    id,
+                    img_cover,
+                    created_by,
+                    prize,
+                    created_date,
+                    tutorial_date
+                FROM (
+                    SELECT tutorial.title,
+                            tutorial.id,
+                            tutorial.img_cover,
+                            tutorial.created_by,
+                            to_char(tutorial.prize, '999,999,999') AS prize,
+                            to_char(tutorial.created_date, 'Month DD,YYYY') AS created_date,
+                            tutorial.created_date AS tutorial_date,
+                            liked_tutorial.id as id_like_tutorial, 
+                            purchased_tutorial.id as id_purchase_tutorial
+                    FROM tutorial
+                    FULL OUTER JOIN liked_tutorial ON tutorial.id=liked_tutorial.tutorial_id
+                    FULL OUTER JOIN purchased_tutorial ON tutorial.id = purchased_tutorial.tutorial_id
+                    WHERE tutorial.is_revoke = 'N'
+                ) AS tbl_tutorial
+                GROUP BY title,
+                id,
+                img_cover,
+                created_by,
+                prize,
+                created_date,
+                tutorial_date
+                ORDER BY total_purchase DESC, tutorial_date DESC
+                LIMIT :tutorialsPerPage OFFSET :startIndexOfMoreTutorials
+            ";
+
+            $tutorials = R::getAll($query, [':tutorialsPerPage' => TUTORIALS_PER_PAGE, ':startIndexOfMoreTutorials' => $startIndexOfMoreTutorials]);
+
+            $tutorials = $this->shortenTitle($tutorials);
+
+            return $tutorials;
+        } else {
+            $tutorials = array();
+            return $tutorials;
+        }
+    }
 }
