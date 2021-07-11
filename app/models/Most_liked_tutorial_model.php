4698f9423879e3228381584878b8fde92bab75bd
widygui93 <guilias.widy@gmail.com>
2021-07-11 19:46:13 +0700

    buat file Most_liked_tutorial_model lalu query ny sama seperti best_seller_tutorial_model tapi order by ny dgn total_like

---
+++ app/models/Most_liked_tutorial_model.php
@@ -0,0 +1,59 @@
+<?php
+class Most_liked_tutorial_model extends Model
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getMostLikedTutorials(): array
+    {
+        $numOfTutorials = R::count('tutorial');
+        if ($numOfTutorials > 0) {
+            $query = "
+                SELECT 
+                    COUNT(id_like_tutorial) AS total_like,
+                    title,
+                    id,
+                    img_cover,
+                    created_by,
+                    prize,
+                    created_date,
+                    tutorial_date
+                FROM (
+                    SELECT tutorial.title,
+                            tutorial.id,
+                            tutorial.img_cover,
+                            tutorial.created_by,
+                            to_char(tutorial.prize, '999,999,999') AS prize,
+                            to_char(tutorial.created_date, 'Month DD,YYYY') AS created_date,
+                            tutorial.created_date AS tutorial_date,
+                            liked_tutorial.id as id_like_tutorial
+                    FROM tutorial
+                    FULL OUTER JOIN liked_tutorial ON tutorial.id=liked_tutorial.tutorial_id
+                    WHERE tutorial.is_revoke = 'N'
+                ) AS tbl_tutorial
+                GROUP BY title,
+                id,
+                img_cover,
+                created_by,
+                prize,
+                created_date,
+                tutorial_date
+                ORDER BY total_like DESC , tutorial_date DESC
+                LIMIT " . TUTORIALS_PER_PAGE . "
+            ";
+
+            $tutorials = R::getAll($query);
+
+            $tutorials = $this->shortenTitle($tutorials);
+
+            return $tutorials;
+        } else {
+            $tutorials = array();
+            return $tutorials;
+        }
+    }
+}
