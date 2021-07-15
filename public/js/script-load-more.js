7638241041461b69d8e55e230ff7d19e7df87014
widygui93 <guilias.widy@gmail.com>
2021-07-15 19:05:54 +0700

    tambahkan interceptor request sehingga sebelum request maka ubah kata more jadi wait, buat button jadi disable dan cursor jadi wait. tambahkan juga interceptor response untuk remove attribute disable dan style sehingga balik ke kondisi awal. dan hapus delay dari function setTimout

--- public/js/script-load-more.js
+++ public/js/script-load-more.js
@@ -11,6 +11,36 @@ $(function () {
     if (currentDisplayTotalTutorial < allTutorials) {
       $("#current-display-total-tutorial").val(currentDisplayTotalTutorial);
 
+      // declare a request interceptor
+      axios.interceptors.request.use(
+        (config) => {
+          // perform a task before the request is sent
+          $(".load-more").text("Wait");
+          $(".load-more").attr("disabled", "true");
+          $(".load-more").css("cursor", "wait");
+          return config;
+        },
+        (error) => {
+          // handle the error
+          return Promise.reject(error);
+        }
+      );
+
+      // declare a response interceptor
+      axios.interceptors.response.use(
+        (response) => {
+          // do something with the response data
+          $(".load-more").removeAttr("disabled");
+          $(".load-more").removeAttr("style");
+
+          return response;
+        },
+        (error) => {
+          // handle the response error
+          return Promise.reject(error);
+        }
+      );
+
       axios({
         method: "post",
         url: "http://localhost/widy/project/dummy_project_3/public/More_tutorial",
@@ -20,21 +50,18 @@ $(function () {
         },
       })
         .then((response) => {
-          // Setting little delay while displaying new content
-          setTimeout(function () {
-            $(".tutorial-wrap").append(response.data);
+          $(".tutorial-wrap").append(response.data);
 
-            let totalTutorialsAfterMore =
-              currentDisplayTotalTutorial + tutorialsperpage;
+          let totalTutorialsAfterMore =
+            currentDisplayTotalTutorial + tutorialsperpage;
 
-            // checking row value is greater than allcount or not
-            if (totalTutorialsAfterMore >= allTutorials) {
-              // Change the text and background
-              $(".load-more").text("Hide");
-            } else {
-              $(".load-more").text("More");
-            }
-          }, 1000);
+          // checking row value is greater than allcount or not
+          if (totalTutorialsAfterMore >= allTutorials) {
+            // Change the text and background
+            $(".load-more").text("Hide");
+          } else {
+            $(".load-more").text("More");
+          }
         })
         .catch((err) => console.error(err));
     } else {
