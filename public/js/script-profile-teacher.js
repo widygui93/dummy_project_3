6af13562c98277ffb593b3155610c0c08591949b
widygui93 <guilias.widy@gmail.com>
2021-08-24 21:39:33 +0700

    ketika button edit profile pic di klik maka ubah display jadi block, dan ketika klik close juga di balikkan display ny jadi none lagi

--- public/js/script-profile-teacher.js
+++ public/js/script-profile-teacher.js
@@ -4,16 +4,32 @@ $(function () {
     $(".modal-content").addClass("shown-modal-content");
   });
 
+  $(document).on("click", "#edit-profile-pic", function () {
+    $(".modalEditProfilePic").css("display", "block");
+    $(".modal-content").addClass("shown-modal-content");
+  });
+
   $(".close-change-password").click(function () {
     $(".modalChangePassword").css("display", "none");
     $(".modal-content").removeClass("shown-modal-content");
   });
 
+  $(".close-edit-profile-pic").click(function () {
+    $(".modalEditProfilePic").css("display", "none");
+    $(".modal-content").removeClass("shown-modal-content");
+  });
+
   // When the user clicks anywhere outside of the modal, close it
   const modalChangePassword = $(".modalChangePassword")[0];
+  const modalEditProfilePic = $(".modalEditProfilePic")[0];
   $(window).click(function (event) {
     if (event.target == modalChangePassword) {
       $(".modalChangePassword").css("display", "none");
+      $(".modal-content").removeClass("shown-modal-content");
+    }
+
+    if (event.target == modalEditProfilePic) {
+      $(".modalEditProfilePic").css("display", "none");
       $(".modal-content").removeClass("shown-modal-content");
     }
   });
