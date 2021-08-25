aee7252cff3d87ec0c07244a6323159ad7ee1ad7
widygui93 <guilias.widy@gmail.com>
2021-08-25 20:50:52 +0700

    tambahkan method editProfilePic beserta method penjagaan seperti isViolateMaxSize, isViolateFileExtention, upload dan getTeacherID

--- app/models/Profile_teacher_model.php
+++ app/models/Profile_teacher_model.php
@@ -83,9 +83,7 @@ class Profile_teacher_model extends Model
             // enkripsi password
             $newEncryptedPass = password_hash($newPassword, PASSWORD_DEFAULT);
 
-            $query = "SELECT id FROM teacher WHERE username = :username";
-            $teacher = R::getAll($query, [':username' => strtolower($_SESSION["username_teacher"])]);
-            $teacherID = $teacher[0]['id'];
+            $teacherID = $this->getTeacherID();
 
             $updatedTeacher = R::load('teacher', $teacherID);
             $updatedTeacher->password = $this->purify($newEncryptedPass);
@@ -108,5 +106,104 @@ class Profile_teacher_model extends Model
                 ];
             }
         }
+    }
+
+    public function editProfilePic(array $data): array
+    {
+        if ($this->isDataEmpty($data)) {
+            return [
+                'icon' => 'error',
+                'title' => 'Failed',
+                'text' => 'Data must be sent'
+            ];
+        } elseif (!$this->doesMandatoryDataFilled(array(
+            "profile-pic" => $_FILES['profile-pic']['name']
+        ))) {
+            return [
+                'icon' => 'error',
+                'title' => 'Failed',
+                'text' => 'Image profile picture is mandatory'
+            ];
+        } elseif ($this->isViolateMaxSize($_FILES['profile-pic']['size'], 200000)) {
+            return [
+                'icon' => 'error',
+                'title' => 'Failed',
+                'text' => 'Max profile picture size is 200 KB'
+            ];
+        } elseif ($this->isViolateFileExtention($_FILES['profile-pic']['name'], ['jpg', 'jpeg', 'png'])) {
+            return [
+                'icon' => 'error',
+                'title' => 'Failed',
+                'text' => 'You upload incorrect image extention'
+            ];
+        } else {
+            $profilePic = $this->upload("../app/core/profile-picture/teacher/", $_FILES['profile-pic']['tmp_name'], $_FILES['profile-pic']['name']);
+
+            $teacherID = $this->getTeacherID();
+
+            $updatedTeacher = R::load('teacher', $teacherID);
+            $updatedTeacher->profile_pic = $this->purify($profilePic);
+
+            try {
+                if (R::store($updatedTeacher)) {
+                    return [
+                        'icon' => 'success',
+                        'title' => 'Success',
+                        'text' => 'Edit profile picture successfully'
+                    ];
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception $e) {
+                return [
+                    'icon' => 'error',
+                    'title' => 'Failed',
+                    'text' => $e->getMessage()
+                ];
+            }
+        }
+    }
+
+    private function isViolateMaxSize(int $fileSize, int $maxSize): bool
+    {
+        return $fileSize > $maxSize ? true : false;
+    }
+
+    private function isViolateFileExtention(string $namaFile, array $validExtention): bool
+    {
+        $ekstensiGambar = explode('.', $namaFile);
+        $ekstensiGambar = strtolower(end($ekstensiGambar));
+        return in_array($ekstensiGambar, $validExtention) ? false : true;
+    }
+
+    private function upload($folder, $tmpName, $namaFile)
+    {
+        $ekstensiGambar = explode('.', $namaFile);
+        $ekstensiGambar = strtolower(end($ekstensiGambar));
+        // generate nama gambar baru
+        $namaFileBaru = $this->createRandomString();
+        $namaFileBaru .= '.';
+        $namaFileBaru .= $ekstensiGambar;
+
+        try {
+            if (move_uploaded_file($tmpName, $folder . $namaFileBaru)) {
+                return $namaFileBaru;
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception $e) {
+            return [
+                'icon' => 'error',
+                'title' => 'Failed',
+                'text' => 'Edit profile picture failed'
+            ];
+        }
+    }
+
+    private function getTeacherID(): string
+    {
+        $query = "SELECT id FROM teacher WHERE username = :username";
+        $teacher = R::getAll($query, [':username' => strtolower($_SESSION["username_teacher"])]);
+        return $teacher[0]['id'];
     }
 }
