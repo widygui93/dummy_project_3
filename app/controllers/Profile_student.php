91a4e6537ef5b32c90fa3b347c049b1a11f734a9
widygui93 <guilias.widy@gmail.com>
2021-09-04 22:54:13 +0700

    buat controller utk profile student

---
+++ app/controllers/Profile_student.php
@@ -0,0 +1,65 @@
+<?php
+class Profile_student extends Controller
+{
+    public function viewProfile(string $username = "")
+    {
+        if (!$this->model('Verify_model')->isUserLogin()) {
+
+            header('Location: ' . BASEURL . '/Login/student');
+            exit;
+        }
+
+        if (!$this->model('Verify_model')->isLoginAsStudent()) return $this->model('Verify_model')->goHome();
+        if ($_SESSION["username_student"] != stripslashes(htmlspecialchars(strtolower($username)))) return $this->model('Verify_model')->goHome();
+
+        $data['profile-student'] = $this->model('Profile_student_model')->getProfileInfoBy($_SESSION["username_student"]);
+        if (empty($data['profile-student'])) return $this->model('Verify_model')->goHome();
+
+
+        $data['style'] = BASEURL . '/css/profile-student-style.css';
+        // hapus salah satu script profile karena isi ny sama aja antara student dan teacher
+        $data['script'] = BASEURL . '/js/script-profile-student.js';
+        $twig = $this->view();
+        echo $twig->render(
+            '/templates/header.html.twig',
+            [
+                'style' => $data['style'],
+                'BASEURL' => BASEURL,
+                'login_teacher' => $_SESSION['login_teacher'] ?? false,
+                'login_student' => $_SESSION['login_student'] ?? false,
+                'username_teacher' => $_SESSION['username_teacher'] ?? '',
+                'username_student' => $_SESSION['username_student'] ?? ''
+            ]
+        );
+
+        echo $twig->render(
+            '/profile/student.html.twig',
+            [
+                'profile_student' => $data['profile-student'],
+                'flash' => Flasher::flash() ?? '',
+                'BASEURL' => BASEURL
+
+            ]
+        );
+
+        echo $twig->render('/templates/footer.html.twig', ['script' => $data['script'], 'BASEURL' => BASEURL]);
+    }
+
+    public function changePassword()
+    {
+        if (!$this->model('Verify_model')->isLoginAsStudent()) return $this->model('Verify_model')->goHome();
+        $result = $this->model('Profile_student_model')->changePassword($_POST);
+        Flasher::setFlash($result['icon'], $result['title'], $result['text']);
+        header('Location: ' . BASEURL . '/Profile_student/viewProfile/' . $_SESSION["username_student"]);
+        exit;
+    }
+
+    public function editProfilePic()
+    {
+        if (!$this->model('Verify_model')->isLoginAsStudent()) return $this->model('Verify_model')->goHome();
+        $result = $this->model('Profile_student_model')->editProfilePic($_POST);
+        Flasher::setFlash($result['icon'], $result['title'], $result['text']);
+        header('Location: ' . BASEURL . '/Profile_student/viewProfile/' . $_SESSION["username_student"]);
+        exit;
+    }
+}
