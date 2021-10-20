38546497d77ce9e0635de54df9d6281d5b78182d
Widy Guilias <guilias.widy@gmail.com>
2021-10-20 09:40:07 +0700

    ganti method view jadi viewCart agar tidak rekursif pas panggil view twig. panggil method getCartDataBy dan getTotalTutorialsInCart. panggil view yg di perlukan utk cart

--- app/controllers/Cart.php
+++ app/controllers/Cart.php
@@ -17,7 +17,7 @@ class Cart extends Controller
         exit;
     }
 
-    public function view()
+    public function viewCart()
     {
         if (!$this->model('Verify_model')->isUserLogin()) {
 
@@ -27,24 +27,21 @@ class Cart extends Controller
 
         if (!$this->model('Verify_model')->isLoginAsStudent()) return $this->model('Verify_model')->goHome();
 
-        $data['cart'] = $this->model('Cart_model')->getCartDataBy();
-        if (empty($data['cart'])) return $this->model('Verify_model')->goHome();
-
-
-        $data['style'] = BASEURL . '/css/cart-style.css';
+        $data['style'] = BASEURL . '/css/tutorial-identifier-style.css';
         $data['style-tutorial'] = BASEURL . '/css/tutorial-style.css';
-        // $data['script'] = BASEURL . '/js/script-home.js';
         $data['script-modal-detail-tutorial'] = BASEURL . '/js/script-modal-detail-tutorial.js';
         $data['script-axios'] = 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
         $data['script-load-more'] = BASEURL . '/js/script-load-more.js';
+        $data['cart'] = $this->model('Cart_model')->getCartDataBy();
+        $data['total-cart'] = $this->model('Cart_model')->getTotalTutorialsInCart();
+        if (empty($data['cart'])) return $this->model('Verify_model')->goHome();
 
-        // $data['style'] = BASEURL . '/css/cart-style.css';
-        // $data['script'] = BASEURL . '/js/script-profile.js';
         $twig = $this->view();
         echo $twig->render(
             '/templates/header.html.twig',
             [
                 'style' => $data['style'],
+                'style_tutorial' => $data['style-tutorial'],
                 'BASEURL' => BASEURL,
                 'login_teacher' => $_SESSION['login_teacher'] ?? false,
                 'login_student' => $_SESSION['login_student'] ?? false,
@@ -54,15 +51,29 @@ class Cart extends Controller
         );
 
         echo $twig->render(
+            '/tutorial/bases/tutorial-identifier.html.twig',
+            ['tutorial_identifier' => 'Cart']
+        );
+
+        echo $twig->render(
             '/tutorial/student/cart/tutorial.html.twig',
             [
-                'cart' => $data['cart'],
+                'tutorials' => $data['cart'],
+                'total_tutorials' => $data['total-cart'],
                 'flash' => Flasher::flash() ?? '', // utk cancel
                 'BASEURL' => BASEURL
 
             ]
         );
 
-        echo $twig->render('/templates/footer.html.twig', ['BASEURL' => BASEURL]);
+        echo $twig->render(
+            '/templates/footer.html.twig',
+            [
+                'script_axios' => $data['script-axios'],
+                'script_modal_detail_tutorial' => $data['script-modal-detail-tutorial'],
+                'script_load_more' => $data['script-load-more'],
+                'BASEURL' => BASEURL
+            ]
+        );
     }
 }
